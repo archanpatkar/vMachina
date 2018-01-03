d3b16d2fd2a05f2476f3ccd6be9d574e5e808254
Archan Patkar <archanpatkar@gmail.com>
2018-01-03 14:51:47 +0530

    Update

--- Operations.js
+++ Operations.js
@@ -1,11 +1,41 @@
 var Operations = 
 {
-    "push":"",
-    "pop":"",
-    "add":"",
-    "sub":"",
-    "mul":"",
-    "div":"",
-    "gt":"",
-    "lt":""
+    "add":function(stack)
+    {
+        let n1 = stack.pop();
+        let n2 = stack.pop();
+        stack.push(n2+n1);
+    },
+    "sub":function(stack)
+    {
+        let n1 = stack.pop();
+        let n2 = stack.pop();
+        stack.push(n2-n1);
+    },
+    "mul":function(stack)
+    {
+        let n1 = stack.pop();
+        let n2 = stack.pop();
+        stack.push(n2*n1);
+    },
+    "div":function(stack)
+    {
+        let n1 = stack.pop();
+        let n2 = stack.pop();
+        stack.push(n2/n1);
+    },
+    "gt":function(stack)
+    {
+        let n1 = stack.pop();
+        let n2 = stack.pop();
+        stack.push((n2 > n1));
+    },
+    "lt":function(stack)
+    {
+        let n1 = stack.pop();
+        let n2 = stack.pop();
+        stack.push((n2 < n1));
+    }
 };
+
+module.exports = Operations;
