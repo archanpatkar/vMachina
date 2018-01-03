5dd4eaef4c1a0fd7826e6881538ddd6ee70e1819
Archan Patkar <archanpatkar@gmail.com>
2018-01-04 05:27:31 +0530

    Update

--- Operations.js
+++ Operations.js
@@ -1,39 +1,57 @@
 var Operations = 
 {
+    "if":function(stack,instructions)
+    {
+        console.log("in if <----------------------------->");
+        let ifblock = stack.pop();
+        console.log(ifblock);
+        let condition = (stack.pop() == "true");
+        if(condition)
+        {
+            ifblock = ifblock.trim();
+            ifblock = ifblock.replace(/[{}]/g, "");
+            ifblock = ifblock.split(";");
+            console.log(ifblock)
+            for(let comm of ifblock)
+            {
+                instructions.push(comm)
+            }
+        }
+    },
     "add":function(stack)
     {
-        let n1 = stack.pop();
-        let n2 = stack.pop();
+        let n1 = Number(stack.pop());
+        let n2 = Number(stack.pop());
         stack.push(n2+n1);
     },
     "sub":function(stack)
     {
-        let n1 = stack.pop();
-        let n2 = stack.pop();
+        let n1 = Number(stack.pop());
+        let n2 = Number(stack.pop());
         stack.push(n2-n1);
     },
     "mul":function(stack)
     {
-        let n1 = stack.pop();
-        let n2 = stack.pop();
+        let n1 = Number(stack.pop());
+        let n2 = Number(stack.pop());
         stack.push(n2*n1);
     },
     "div":function(stack)
     {
-        let n1 = stack.pop();
-        let n2 = stack.pop();
+        let n1 = Number(stack.pop());
+        let n2 = Number(stack.pop());
         stack.push(n2/n1);
     },
     "gt":function(stack)
     {
-        let n1 = stack.pop();
-        let n2 = stack.pop();
+        let n1 = Number(stack.pop());
+        let n2 = Number(stack.pop());
         stack.push((n2 > n1));
     },
     "lt":function(stack)
     {
-        let n1 = stack.pop();
-        let n2 = stack.pop();
+        let n1 = Number(stack.pop());
+        let n2 = Number(stack.pop());
         stack.push((n2 < n1));
     }
 };
