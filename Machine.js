7237e0f42d01169fcd0662677ea4a6d8dae115c9
Archan Patkar <archanpatkar@gmail.com>
2018-01-03 14:51:38 +0530

    Update

--- Machine.js
+++ Machine.js
@@ -5,14 +5,40 @@ class vMachina
 {
     constructor(operations,instructions)
     {
-        this.stack = Stack();
-        this.instructions = Instructions(instructions);
+        this.stack = new Stack();
+        // this.instructions = new Instructions(instructions);
+        this.instructions = instructions;
         this.operations = operations;
     }
 
     execute()
     {
-
+        for(let ins of this.instructions)
+        {
+            if(ins.startsWith("push"))
+            {
+                let arr = ins.split("->");
+                if(arr[0] == "push")
+                {
+                    this.stack.push(Number(arr[1]));
+                }
+            }
+            else if(ins.startsWith("print"))
+            {
+                for(let i = this.stack.st; i >= 0 ; i--)
+                {
+                    console.log("----")
+                    console.log(this.stack.stk[i])
+                    console.log("----")
+                }
+            }
+            else
+            {
+                this.stack.visit(this.operations[ins]);
+            }
+        }
     }
 
 }
+
+module.exports = vMachina;
