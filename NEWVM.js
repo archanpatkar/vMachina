691d5a6a266cc33fe8856982794882fe3a76e223
archanpatkar <pat@archanpatkar.com>
2019-01-07 01:30:14 +0530

    Update

--- NEWVM.js
+++ NEWVM.js
@@ -128,38 +128,4 @@ class vMachina
     }
 }
 
-
-const m = new vMachina();
-
-m.run([
-    ["push",10],
-    ["push",20],
-    ["add"],
-    ["print"]
-]);
-
-console.log("Storing and Loading from memory");
-m.run([
-    ["push",0],
-    ["push",100],
-    ["store"],
-    ["push",0],
-    ["load"],
-    ["print"]
-]);
-
-console.log("Testing Call");
-m.run([
-    ["push",10],
-    ["push",8],
-    ["call"],
-    ["push",10],
-    ["push",20],
-    ["add"],
-    ["print"],
-    ["halt"],
-    ["push",2],
-    ["multiply"],
-    ["print"],
-    ["ret"]
-]);
+module.exports = vMachina;
