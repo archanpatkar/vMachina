7fa21b1a325fff6cbcaeb635aec18febf2bd9a84
archanpatkar <pat@archanpatkar.com>
2019-01-07 01:32:22 +0530

    Update

---
+++ backyard/Main.js
@@ -0,0 +1,14 @@
+var Parser = require("./Parser");
+
+var Operations = require("./Operations");
+
+var vMachina = require("./Machine")
+
+let vm = new vMachina(Operations,Parser.parse(__dirname+"/test2.patcode"));
+
+vm.execute();
+
+console.log("Execution Complete!");
+console.log("----Execution Stats----");
+console.log(vm.stack);
+console.log(vm.instructions);
